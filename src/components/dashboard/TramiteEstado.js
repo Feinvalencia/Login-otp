a7b917633f35a1ca0f60e602f6475e00e383e447
feiner <feinvalencia@gmail.com>
2021-11-11 11:35:50 -0500

    optimizacion de codigo

---
+++ src/components/dashboard/TramiteEstado.js
@@ -0,0 +1,40 @@
+import React from "react";
+const TramiteEstado1 = styled(Tramite)`
+  margin-bottom: 5rem;
+  background-color: #0097cb8a;
+  backdrop-filter: blur(12px);
+  -webkit-backdrop-filter: blur(12px);
+  h3 {
+    font-size: 2rem;
+  }
+`;
+const TramiteEstado3 = styled(Tramite)`
+  background-color: #55eba999;
+  backdrop-filter: blur(12px);
+  -webkit-backdrop-filter: blur(12px);
+  margin-top: 5rem;
+  h3 {
+    font-size: 2rem;
+  }
+`;
+const TramiteEstado2 = styled(Tramite)`
+  background-color: #1ae3d699;
+  backdrop-filter: blur(12px);
+  -webkit-backdrop-filter: blur(12px);
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+  h3 {
+    font-size: 2rem;
+  }
+`;
+const TramiteEstado4 = styled(Tramite)`
+  background-color: #2dcb008a;
+  backdrop-filter: blur(12px);
+  -webkit-backdrop-filter: blur(12px);
+  h3 {
+    font-size: 2rem;
+  }
+`;
+export const TramiteEstado = ({ align }) => {
+  return <div></div>;
+};
